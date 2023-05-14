da74cdbb43bd98cb28f1be6d6416805b4fdc15f1
fahad <fahadthereal666@gmail.com>
2023-05-14 06:18:09 +0500

    first commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use App\Models\Company;
+use App\Models\Contact;
+
+
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Company::factory()->count(10)->create();
+        Contact::factory()->count(50)->create();
+        User::factory()->count(2)->create();
+    }
+}
